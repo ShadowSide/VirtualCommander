a3372f0d46ab3f62181952d2a3369ee6dad20bcc
SergeyZhuravlev <sergey4zhuravlev@gmail.com>
2016-12-08 23:30:10 +0300

    empty

---
+++ VE/VirtualEngine.cpp
@@ -0,0 +1,1 @@
+#include "VE/VirtualEngine.h"
---
+++ VE/VirtualEngine.h
@@ -0,0 +1,30 @@
+#pragma once
+
+//class FileSystemReader;
+
+class FsItem;//FsNode
+
+class FsChangeWatcher;
+
+class FileSystem
+{
+
+};
+
+class FsDrivesReader;
+
+class FsPlanner;
+
+class FsOperation;
+
+class FsOpSync /*: FsOperation*/;
+
+class FsOpCopy /*: FsOperation*/;
+
+class FsOpMove /*: FsOperation*/;
+
+class FsOpQueue;
+
+class FsContext;
+
+class TwoSidePanels;
